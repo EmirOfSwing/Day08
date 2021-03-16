b04b8d8c372ffa7ad51ce51fd1e6caeb61ee5808
EmirOfSwing <emir.arh@gmail.com>
2021-03-16 08:19:33 +0100

    Added exercises for Day08

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+addThree;
+console.log(addThree());
+
+function addFive() {
+    sum = sum + 5;
+  }
+  addFive;
+  console.log(addFive());
+
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
